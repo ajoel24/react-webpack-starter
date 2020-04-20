45f47095a037b62ecee8d6d67e1dd7f061bb4303
Andrew2432 <drew.devops@gmail.com>
2020-04-20 14:51:30 +0530

    Remove sass dependency

--- webpack.config.js
+++ webpack.config.js
@@ -24,10 +24,6 @@ module.exports = {
 				test: /\.css$/,
 				use: ["style-loader", "css-loader"],
 			},
-			{
-				test: /\.scss$/,
-				loaders: ExtractTextPlugin.extract("css!sass"),
-			},
 		],
 	},
 	mode: "development",
