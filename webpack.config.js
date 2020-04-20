a476c1b83c718e7940783538d140521c0d620011
Andrew2432 <drew.devops@gmail.com>
2020-04-20 14:27:37 +0530

    Finish build

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+module.exports = {
+	entry: "./src/index.js",
+	output: {
+		path: path.join(__dirname, "/dist"),
+		filename: "index.bundle.js",
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+				resolve: {
+					extensions: [".js", ".jsx"],
+				},
+				use: {
+					loader: "babel-loader",
+				},
+			},
+			{
+				test: /\.css$/,
+				use: ["style-loader", "css-loader"],
+			},
+			{
+				test: /\.scss$/,
+				loaders: ExtractTextPlugin.extract("css!sass"),
+			},
+		],
+	},
+	mode: "development",
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: "./src/index.html",
+		}),
+		new ExtractTextPlugin("dist/styles/main.css", {
+			allChunks: true,
+		}),
+	],
+};
