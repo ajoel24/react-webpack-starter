14779742ea4b6e60366e2b5b2cee0c289c0e4004
Andrew2432 <drew.devops@gmail.com>
2020-04-26 11:52:05 +0530

    Add image loader

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,8 +1,14 @@
-import React from "react";
-import "./App.css";
+import React from 'react';
+import './App.css';
+import image from '../../assets/images/image1.webp';
 
 function App() {
-	return <h1>Hello</h1>;
+  return (
+    <main>
+      <h1>Hello</h1>
+      <img src={image} alt="Sample image" />
+    </main>
+  );
 }
 
 export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,44 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
-	entry: "./src/index.js",
-	output: {
-		path: path.join(__dirname, "/dist"),
-		filename: "index.bundle.js",
-	},
-	module: {
-		rules: [
-			{
-				test: /\.(js|jsx)$/,
-				exclude: /node_modules/,
-				resolve: {
-					extensions: [".js", ".jsx"],
-				},
-				use: {
-					loader: "babel-loader",
-				},
-			},
-			{
-				test: /\.css$/,
-				use: ["style-loader", "css-loader"],
-			},
-		],
-	},
-	mode: "development",
-	plugins: [
-		new HtmlWebpackPlugin({
-			template: "./src/index.html",
-		}),
-		new ExtractTextPlugin("dist/styles/main.css", {
-			allChunks: true,
-		}),
-	],
+  entry: './src/index.js',
+  output: {
+    path: path.join(__dirname, '/dist'),
+    filename: 'index.bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        resolve: {
+          extensions: ['.js', '.jsx'],
+        },
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(jpg|png|webp)$/,
+        use: {
+          loader: 'url-loader',
+        },
+      },
+    ],
+  },
+  mode: 'development',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+    new ExtractTextPlugin('dist/styles/main.css', {
+      allChunks: true,
+    }),
+  ],
 };
